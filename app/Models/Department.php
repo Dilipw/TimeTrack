01994aca1448c2b48ce89982e83f72011c103aaa
Dilip Waghmare <dilipwaghmarep15@gmail.com>
2026-09-11 12:28:50 +0530

    feat: create Department model and migration for departments table

---
+++ app/Models/Department.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\DepartmentFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#[Fillable(['name', 'description'])]
+class Department extends Model
+{
+    /** @use HasFactory<DepartmentFactory> */
+    use HasFactory, SoftDeletes;
+}
